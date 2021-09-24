be56361a25c7ceebcc17cb2efbda9f1a01c6a6e1
Maya Borrero <mlbsindia@gmail.com>
2021-09-24 12:49:51 -0400

    commit tree 699cdab2ca490da25b160be378dcaf6a873fa18a

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+// var bestCustomer = 'not bob';
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'jill';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'john';
+}
